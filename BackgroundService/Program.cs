ac589a6b46fd7172fc5af2639067248f9d03db00
TheDayIsMyEnemy <34052076+TheDayIsMyEnemy@users.noreply.github.com>
2023-05-26 17:20:25 +0300

    Background Service Template

---
+++ BackgroundService/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace BackgroundService
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            using var host = Startup.BuildHost(args);
+            await host.StartAsync();
+            var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+
+            var services = host.Services;
+
+            var config = services.GetRequiredService<IConfiguration>();
+            var env = services.GetRequiredService<IHostEnvironment>();
+            var logger = services.GetRequiredService<ILogger<Program>>();
+        }
+    }
+}
